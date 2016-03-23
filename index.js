b8bd863a2563fd4dbc93092e7b6f6d0e118aa898
AJ Beckner <ajbeckner@gmail.com>
2016-03-23 16:40:37 -0400

    added commenting

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//SERVER File for MEAN Opera App
+//todo: separate into multiple files
+
 //Require Dependencies
 var http = require('http');
 var express = require('express');
@@ -5,59 +8,63 @@ var bodyParser = require('body-parser')
 var socketIO = require('socket.io');
 var mongoose = require('mongoose');
 
-//Server Setup
+//Set up Server to use both express for REST and socket.io for real time socket programming
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 //MONGOOSE
-//connect to db
+// mongoose is a middleware for mongodb that enforces structured data and allows db to be taken care of in node
+// connect to db
 mongoose.connect('mongodb://localhost/Opera');
 //create schema to enforce for db
 var Schema = mongoose.Schema;
+//set up simple schema for messages; each entry will look like this JSON: {text: String}
 var MessageSchema = new Schema({text: String},{ minimize: false });
 var Message = mongoose.model('Message', MessageSchema);
 
-//HTML Views
-//display
+//return the HTML Views for each angular app when requested
+//display angular app
 app.get('/display', function(req, res){
   res.sendFile(__dirname +'/display.html');
 });
-//editor
+//editor angular app
 app.get('/editor', function(req, res){
   res.sendFile(__dirname +'/editor.html');
 });
 
 //EXPRESS
+//express is a routing middleware for node that allows you to manipulate data more easily with different modules
 
+//set up bodyparser
 app.use(bodyParser.json());
 
 //GET all messages fom db
 app.get('/all', function(req,res) {
+  //call to mongo instance (blank query == all)
   Message.find({}).then(function(dbMessages) {
     res.json(dbMessages);
   });
 });
 
-// POST messages
+// POST messages to db
 app.post('/add', function(req,res) {
+
+  //enforce schema by assigning each prop manually
+  var message = new Message();
+  var prop;
+  for (prop in req.body) {
+    message[prop] = req.body[prop];
+  }
+
   if (req.body.hasOwnProperty("_id")){
     //if _id included, update existing message
-    var message = new Message();
-    var prop;
-    for (prop in req.body) {
-      message[prop] = req.body[prop];
-    }
     Message.update({_id: req.body._id}, message).then(function() {
       res.json({'status': 'updated','message':message});
     });
   } else {
     //if _id not included, add new message
-    var message = new Message();
-    var prop;
-    for (prop in req.body) {
-      message[prop] = req.body[prop];
-    }
+    //NOTE: .save() is called on the schema obj instance NOT the db (lowercase m)
     message.save().then(function(dbMessage) {
       res.json({'status': 'added','message':dbMessage});
     });
@@ -79,12 +86,13 @@ app.post('/delete', function(req,res) {
 //GET to forward next message
 app.get('/push/:index', function(req,res) {
   Message.find({}).then(function(dbMessages) {
+    //send socket.io message to all clients
     io.emit('toDisplay',{message: dbMessages[req.params.index].text});
   });
   res.json({status:"sent"});
 });
 
-//SOCKET.IO
+//SOCKET.IO Set up
 io.on('connection', function(socket)
 {
   console.log('Client connected.');
